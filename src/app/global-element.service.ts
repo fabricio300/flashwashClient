97dce9820d5c47235732e0a5909d8805dc6cdc0b
Fabricio <rodoflamingo@gmail.com>
2019-12-04 19:27:53 -0600

    ultima actualizacion

--- src/app/global-element.service.ts
+++ src/app/global-element.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class GlobalElementService {
-  api : string = 'https://flash-wash-01.herokuapp.com/api/v1/'
+  api : string = 'https://167.172.209.210/api/v1/'
   status_de_secion=false
 
 
